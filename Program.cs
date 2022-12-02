0edeaf7dfb164abd7f10372de3d1a9322cdd7abc
F.Kiyota <kiyota-f@asahi.com>
2022-12-02 18:10:28 +0900

    Toast app

--- Program.cs
+++ Program.cs
@@ -4,10 +4,32 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using System.Linq;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 await Radio.RequestAccessAsync().AsTask();
 var bluetooth = (await Radio.GetRadiosAsync().AsTask()).Single(r => r.Kind == RadioKind.Bluetooth);
-Console.WriteLine(bluetooth.State);
+var prevState = bluetooth.State;
+
+var stateStr = args.Length < 1 ? "toggle" : args[0];
+
+
+RadioState next;
+if (stateStr.ToLower() == "toggle")
+{
+    if (prevState == RadioState.Off)
+        next = RadioState.On;
+    else
+        next = RadioState.Off;
+}
+else
+    next = (RadioState)Enum.Parse(typeof(RadioState), stateStr, true);
+
+await bluetooth.SetStateAsync(next).AsTask();
+
+new ToastContentBuilder()
+    .AddText($"Bluetooth state {bluetooth.State}")
+    .AddText(prevState == bluetooth.State ? "Not updated." : $"{prevState} → {bluetooth.State}")
+    .Show();
 
 static class Extension
 {
