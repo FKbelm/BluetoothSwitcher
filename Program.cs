b3e15ab8e37924cf8ecc2764539733a258a2e401
F.Kiyota <kiyota-f@asahi.com>
2022-12-02 17:55:17 +0900

    Get bluetooth device

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.Devices.Radios;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using System.Linq;
+
+await Radio.RequestAccessAsync().AsTask();
+var bluetooth = (await Radio.GetRadiosAsync().AsTask()).Single(r => r.Kind == RadioKind.Bluetooth);
+Console.WriteLine(bluetooth.State);
+
+static class Extension
+{
+    public static Task<T> AsTask<T>(this IAsyncOperation<T> operation)
+    {
+        var tcs = new TaskCompletionSource<T>();
+        operation.Completed = delegate
+        {
+            switch (operation.Status)   //--- 状態に合わせて完了通知
+            {
+                case AsyncStatus.Completed: tcs.SetResult(operation.GetResults()); break;
+                case AsyncStatus.Error: tcs.SetException(operation.ErrorCode); break;
+                case AsyncStatus.Canceled: tcs.SetCanceled(); break;
+            }
+        };
+        return tcs.Task;  //--- 完了が通知されるTaskを返す
+    }
+
+}
